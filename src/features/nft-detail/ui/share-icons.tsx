63eba7cb8754ccf6e028cf903f50a6fdd9589c80
manuel <manuelds@outlook.pt>
2026-09-09 20:16:13 +0100

    add related nfts

---
+++ src/features/nft-detail/ui/share-icons.tsx
@@ -0,0 +1,15 @@
+export function LinkedinGlyph(props: React.ComponentProps<'svg'>) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" fill="currentColor" {...props}>
+      <path d="M20.45 20.45h-3.56v-5.57c0-1.33-.02-3.04-1.85-3.04-1.85 0-2.14 1.45-2.14 2.94v5.67H9.34V9h3.41v1.56h.05c.48-.9 1.64-1.85 3.38-1.85 3.6 0 4.27 2.37 4.27 5.46v6.28ZM5.34 7.43a2.06 2.06 0 1 1 0-4.12 2.06 2.06 0 0 1 0 4.12ZM7.12 20.45H3.56V9h3.56v11.45Z" />
+    </svg>
+  )
+}
+
+export function TwitterGlyph(props: React.ComponentProps<'svg'>) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" fill="currentColor" {...props}>
+      <path d="M22 5.9c-.7.32-1.46.53-2.25.63a3.9 3.9 0 0 0 1.71-2.16c-.76.45-1.6.78-2.5.96a3.86 3.86 0 0 0-6.58 3.52A11.06 11.06 0 0 1 4.4 4.7a3.88 3.88 0 0 0 1.2 5.16 3.83 3.83 0 0 1-1.75-.49 3.87 3.87 0 0 0 3.1 3.84 3.85 3.85 0 0 1-1.75.07 3.87 3.87 0 0 0 3.61 2.69A7.76 7.76 0 0 1 2 17.54a10.94 10.94 0 0 0 5.94 1.74c7.13 0 11.03-5.9 11.03-11.03l-.01-.5A7.9 7.9 0 0 0 22 5.9Z" />
+    </svg>
+  )
+}
